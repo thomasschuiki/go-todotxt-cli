8674dcf69198124bbd9263de5ef85858c8589d80
Thomas Eitler <thomas.eitler@gmail.com>
2019-01-19 18:05:38 +0100

    add project files

---
+++ cmd/root.go
@@ -0,0 +1,104 @@
+// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"log"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/JamesClonk/go-todotxt"
+)
+
+var cfgFile string
+var Verbose bool
+var Version string
+var TodoTxtFile todotxt.TaskList
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gotodotxt",
+	Short: "commandline client to interact with todo.txt files",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	var err error
+	// load todo.txt file
+	todotxt.IgnoreComments = false
+	// TODO: use variable provided by config file and a sane default
+	// TODO: provide option to create the file
+	if TodoTxtFile, err = todotxt.LoadFromFilename("todo.txt"); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Init successfull")		
+	}
+	
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gotodotxt.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.	
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "", false, "verbose output")
+	rootCmd.PersistentFlags().StringVarP(&Version, "version", "v", "", "version")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".gotodotxt" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".gotodotxt")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
